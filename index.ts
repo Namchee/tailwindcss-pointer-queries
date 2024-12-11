064cb2e6462bbc5afb9940f3166e718ff4404d22
Cristopher <cristophernamchee12@gmail.com>
2024-12-11 21:38:28 +0700

    feat: add any-pointer queries

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
-import plugin from 'tailwindcss/plugin.js';
+import plugin from 'tailwindcss/plugin';
 
 export default plugin(({ addVariant }) => {
     addVariant('pointer-none', '@media (pointer: none)');
     addVariant('pointer-fine', '@media (pointer: fine)');
     addVariant('pointer-coarse', '@media (pointer: coarse)');
+    addVariant('pointer-any-fine', '@media (any-pointer: fine)');
+    addVariant('pointer-any-coarse', '@media (any-pointer: coarse)');
 });
